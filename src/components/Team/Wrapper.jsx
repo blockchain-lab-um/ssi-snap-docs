9f0cd0e484b7a07d9e55f0e6e14090cc8b9380c2
andyv09 <andraz.vrecko@gmail.com>
2022-07-27 15:50:23 +0200

    no message

--- src/components/Team/Wrapper.jsx
+++ src/components/Team/Wrapper.jsx
@@ -4,17 +4,20 @@ import Members from "./Members";
 
 export default function Wrapper() {
   return (
-    <div className="wrapper">
-      {Members.members.map((member, i) => (
-        <TeamCard
-          name={member.name}
-          title={member.title}
-          description={member.description}
-          linkedin={member.linkedin}
-          image={member.image}
-          twitter={member.twitter}
-        />
-      ))}
+    <div>
+      <h1>Members</h1>
+      <div className="wrapper">
+        {Members.members.map((member, i) => (
+          <TeamCard
+            name={member.name}
+            title={member.title}
+            description={member.description}
+            linkedin={member.linkedin}
+            image={member.image}
+            twitter={member.twitter}
+          />
+        ))}
+      </div>
     </div>
   );
 }
