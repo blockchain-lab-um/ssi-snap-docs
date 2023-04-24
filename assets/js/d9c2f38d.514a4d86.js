3206be5d0fce60585c2b8d3f668eaa1ba0fc43f3
Martin Domajnko <domajnko.martin@gmail.com>
2023-04-24 13:24:41 +0200

    fix: redirect to new domain

--- assets/js/d9c2f38d.514a4d86.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_blockchain_lab_um_ssi_snap_docs=self.webpackChunk_blockchain_lab_um_ssi_snap_docs||[]).push([[244],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
